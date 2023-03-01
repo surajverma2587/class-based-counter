2ce1bd6dbb7a8264424510aa7f0223185f69c68f
surajverma2587 <surajverma2587@gmail.com>
2023-03-01 19:25:38 +0000

    basic hello app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+
+import { App } from "./App";
+
+const reactContainer = document.getElementById("root");
+
+const root = ReactDOM.createRoot(reactContainer);
+
+root.render(<App />);
