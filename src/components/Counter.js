3005a09fa799c0876384b50f7fd40337ab2fd88b
surajverma2587 <surajverma2587@gmail.com>
2023-03-01 19:50:08 +0000

    add counter logic

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,55 @@
-export const Counter = () => {
-  return (
-    <section className="border m-2">
-      <h2 className="text-center p-4">Counter: 0</h2>
+import { Component } from "react";
 
-      <div className="d-flex justify-content-around mb-4">
-        <button className="btn btn-danger">Decrement</button>
-        <button className="btn btn-warning">Reset</button>
-        <button className="btn btn-success">Increment</button>
-      </div>
-    </section>
-  );
-};
+export class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+    };
+
+    this.handleIncrement = this.handleIncrement.bind(this);
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+
+  handleIncrement() {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  }
+
+  handleDecrement() {
+    if (this.state.counter > 0) {
+      this.setState({
+        counter: this.state.counter - 1,
+      });
+    }
+  }
+
+  handleReset() {
+    this.setState({
+      counter: 0,
+    });
+  }
+
+  render() {
+    return (
+      <section className="border m-2">
+        <h2 className="text-center p-4">Counter: {this.state.counter}</h2>
+
+        <div className="d-flex justify-content-around mb-4">
+          <button className="btn btn-danger" onClick={this.handleDecrement}>
+            Decrement
+          </button>
+          <button className="btn btn-warning" onClick={this.handleReset}>
+            Reset
+          </button>
+          <button className="btn btn-success" onClick={this.handleIncrement}>
+            Increment
+          </button>
+        </div>
+      </section>
+    );
+  }
+}
