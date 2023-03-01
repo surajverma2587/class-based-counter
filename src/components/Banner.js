536fc264cad121dd42fe73a06a5b528bd637b606
surajverma2587 <surajverma2587@gmail.com>
2023-03-01 19:36:06 +0000

    complete styled counter

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 export const Banner = () => {
   return (
-    <section className="banner">
+    <section className="banner border m-2">
       <h1>React Counter</h1>
       <h2>Please use the buttons to interact with your counter</h2>
     </section>
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,3 +1,13 @@
 export const Counter = () => {
-  return <div>Counter</div>;
+  return (
+    <section className="border m-2">
+      <h2 className="text-center p-4">Counter: 0</h2>
+
+      <div className="d-flex justify-content-around mb-4">
+        <button className="btn btn-danger">Decrement</button>
+        <button className="btn btn-warning">Reset</button>
+        <button className="btn btn-success">Increment</button>
+      </div>
+    </section>
+  );
 };
