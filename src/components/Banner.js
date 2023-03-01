15b35527e0e906a3e97d9f6c22c99bb6ba269d42
surajverma2587 <surajverma2587@gmail.com>
2023-03-01 19:32:14 +0000

    complete styled banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,3 +1,8 @@
 export const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <section className="banner">
+      <h1>React Counter</h1>
+      <h2>Please use the buttons to interact with your counter</h2>
+    </section>
+  );
 };
